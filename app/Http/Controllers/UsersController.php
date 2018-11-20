3a028c21fc4153af32acc251609be0db12fe746e
liwl0420 <liwl0420@gmail.com>
2018-11-20 06:40:25 +0000

    Finish login and logout

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -7,7 +7,7 @@ use App\Models\User;
 
 class UsersController extends Controller
 {
-    public function create()
+	public function create()
 	{
 		return view('users.create');
 	}
@@ -31,6 +31,7 @@ class UsersController extends Controller
 			'password' => bcrypt($request->password),
 		]);
 
+		Auth::login($user);
 		session()->flash('success', '欢迎，您将在这里开启一段新的旅程~');
 		return redirect()->route('users.show', [$user]);
 	}
